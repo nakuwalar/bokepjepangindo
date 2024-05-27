224bc0a5c824ea32e400f689cb3ed30104360c37
nakuwalar <nakuwalar@gmail.com>
2024-05-28 05:08:00 +0700

    Update navbar.tsx

--- components/layouts/navbar.tsx
+++ components/layouts/navbar.tsx
@@ -146,6 +146,13 @@ const Navbar = async () => {
             </div>
             <div className="flex flex-row space-x-2">
                 <div className="hidden md:flex items-center gap-4">
+                    <Link
+                        href="https://bocilngentot.com"
+                        className="hover:bg-accent px-2 py-1.5 rounded-md"
+                        aria-label="Open liked videos"
+                    >
+                        Bokep Bocil
+                    </Link>
                     <NavMenu folders={folders}></NavMenu>
                 </div>
                 <SearchInput className="hidden md:block w-full md:w-[260px] lg:w-[300px]" />
