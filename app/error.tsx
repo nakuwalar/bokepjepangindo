36457626cd9368d51d8707f628a1c6adf8529ff9
nakuwalar <nakuwalar@gmail.com>
2024-06-09 05:26:10 +0700

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -1,11 +1,14 @@
-"use client"; // Error components must be Client Components
-
+"use client";
+import CardList from "@/components/card-list";
+import { DEFAULT_PER_PAGE } from "@/lib/constants";
+import SearchCardList from "@/components/search/search-list";
+import { SITENAME } from "@/lib/constants";
 import { Button } from "@/components/ui/button";
 import Center from "@/components/layouts/center";
 import MessageBox from "@/components/message-box";
 import { useEffect } from "react";
 
-export default function Error({
+export function Error({
     error,
     reset,
 }: {
@@ -20,5 +23,30 @@ export default function Error({
         <MessageBox title={error.message} countdown={3}>
             <Button onClick={reset}>Try Again</Button>
         </MessageBox>
+    );
+}
+
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
+    const page =
+        (searchParams.page && parseInt(searchParams.page as string)) || 1;
+    const per_page =
+        (searchParams.per_page && parseInt(searchParams.per_page as string)) ||
+        DEFAULT_PER_PAGE;
+    const fld_id =
+        (searchParams.fld_id && (searchParams.fld_id as string)) || undefined;
+    const query = (searchParams.q && (searchParams.q as string)) || undefined;
+
+    return (
+        <div className="md:my-2">
+            {query ? (
+                <SearchCardList query={query} banner />
+            ) : (
+                <CardList page={page} per_page={per_page} fld_id={fld_id} />
+            )}
+        </div>
     );
 }
