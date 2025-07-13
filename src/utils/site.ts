a06ac2c73b7164f795a08847307ff30ca93ce009
nakuwalar <nakuwalar@gmail.com>
2025-07-14 04:14:55 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Jepang Indo';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Jepang Viral Sma Cindo Tante China';
-export const url = 'https://bokepjepangindo.pages.dev';
-export const terbit = '2025-06-25T00:01:03Z';
